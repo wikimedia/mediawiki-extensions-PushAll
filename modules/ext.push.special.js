9b5886fcb5681f2369162efffeaadb5e8699f01e
Reedy <reedy@wikimedia.org>
2018-06-21 23:11:14 +0100

    Remove more trailing whitespace (this time from js files)
    Change-Id: I3690d377c03aa63e3ce5274700fe09e17526f005

--- modules/ext.push.special.js
+++ modules/ext.push.special.js
@@ -1,47 +1,47 @@
 /**
  * JavaScript for Special:Push in the Push extension.
  * @see http://www.mediawiki.org/wiki/Extension:Push
- * 
+ *
  * @author Jeroen De Dauw <jeroendedauw at gmail dot com>
  */
 
-(function($) { $( document ).ready( function() {
-	
+(function( $ ) { $( document ).ready( function() {
+
 	// Compatibility with pre-RL code.
 	// Messages will have been loaded into wgPushMessages.
 	if ( typeof mediaWiki === 'undefined' ) {
 		mediaWiki = new Object();
-		
+
 		mediaWiki.msg = function() {
 			message = window.wgPushMessages[arguments[0]];
-			
+
 			for ( var i = arguments.length - 1; i > 0; i-- ) {
 				message = message.replace( '$' + i, arguments[i] );
 			}
-			
+
 			return message;
 		}
 	}
 
 	var resultList = $('#pushResultList');
-	
+
 	var targets = [];
-	for (targetName in window.wgPushTargets) targets.push( window.wgPushTargets[targetName] ); 
-	
+	for (targetName in window.wgPushTargets) targets.push( window.wgPushTargets[targetName] );
+
 	var pages = window.wgPushPages;
-	
+
 	var requestAmount = Math.min( pages.length, window.wgPushWorkerCount );
 	var batchSize = Math.min( targets.length, window.wgPushBatchSize );
-	
+
 	var pushedFiles = [];
-	
+
 	for ( i = requestAmount; i > 0; i-- ) {
 		initiateNextPush();
 	}
-	
+
 	function initiateNextPush() {
 		var page = pages.pop();
-		
+
 		if ( page ) {
 			startPush( page, 0, null );
 		}
@@ -49,34 +49,34 @@
 			showCompletion();
 		}
 	}
-	
+
 	function appendAndScroll( item ) {
 		var box = $('#pushResultDiv');
 		var innerBox = $('#pushResultDiv > .innerResultBox');
-		var atBottom = Math.abs(innerBox.offset().top) + box.height() + box.offset().top >= innerBox.outerHeight();		
-		
+		var atBottom = Math.abs(innerBox.offset().top) + box.height() + box.offset().top >= innerBox.outerHeight();
+
 		resultList.append( item );
-		
+
 		if ( atBottom ) {
 			box.attr( {'scrollTop': box.attr( 'scrollHeight' )} );
-		}		
+		}
 	}
-	
+
 	function startPush( pageName, targetOffset, listItem ) {
 		if ( targetOffset == 0 ) {
 			var listItem = $( '<li />' );
 			listItem.text( mediaWiki.msg( 'push-special-item-pushing', pageName ) );
 			appendAndScroll( listItem );
 		}
-		
+
 		var currentBatchLimit = Math.min( targetOffset + batchSize, targets.length );
 		var currentBatchStart = targetOffset;
-		
+
 		if ( targetOffset < targets.length ) {
 			listItem.text( listItem.text() + '...' );
-			
+
 			targetOffset = currentBatchLimit;
-			
+
 			$.getJSON(
 				wgScriptPath + '/api.php',
 				{
@@ -91,12 +91,12 @@
 					}
 					else if ( data.length > 0 && data[0].edit && data[0].edit.captcha ) {
 						handleError( listItem, pageName, { info: mediaWiki.msg( 'push-err-captcha-page', pageName ) } );
-					}	
+					}
 					else {
 						startPush( pageName, targetOffset, listItem );
 					}
 				}
-			);			
+			);
 		}
 		else {
 			if ( window.wgPushIncFiles ) {
@@ -107,10 +107,10 @@
 			}
 		}
 	}
-	
+
 	function getIncludedImagesAndInitPush( pageName, listItem ) {
 		listItem.text( mediaWiki.msg( 'push-special-obtaining-fileinfo', pageName ) );
-		
+
 		$.getJSON(
 			wgScriptPath + '/api.php',
 			{
@@ -123,7 +123,7 @@
 			function( data ) {
 				if ( data.query ) {
 					var images = [];
-					
+
 					for ( page in data.query.pages ) {
 						if ( data.query.pages[page].images ) {
 							for ( var i = data.query.pages[page].images.length - 1; i >= 0; i-- ) {
@@ -131,10 +131,10 @@
 									pushedFiles.push( data.query.pages[page].images[i].title );
 									images.push( data.query.pages[page].images[i].title );
 								}
-							}							
+							}
 						}
 					}
-					
+
 					if ( images.length > 0 ) {
 						var currentFile = images.pop();
 						startFilePush( pageName, images, 0, listItem, currentFile );
@@ -147,9 +147,9 @@
 					handleError( pageName, { info: mediaWiki.msg( 'push-special-err-imginfo-failed' ) } );
 				}
 			}
-		);		
+		);
 	}
-	
+
 	function startFilePush( pageName, images, targetOffset, listItem, fileName ) {
 		if ( targetOffset == 0 ) {
 			listItem.text( mediaWiki.msg( 'push-special-pushing-file', pageName, fileName ) );
@@ -157,29 +157,29 @@
 		else {
 			listItem.text( listItem.text() + '...' );
 		}
-		
+
 		var currentBatchLimit = Math.min( targetOffset + batchSize, targets.length );
 		var currentBatchStart = targetOffset;
-		
+
 		if ( targetOffset < targets.length ) {
 			listItem.text( listItem.text() + '...' );
-			
+
 			targetOffset = currentBatchLimit;
-			
+
 			$.getJSON(
 				wgScriptPath + '/api.php',
 				{
 					'action': 'pushimages',
 					'format': 'json',
-					'images': fileName, 
+					'images': fileName,
 					'targets': targets.slice( currentBatchStart, currentBatchLimit ).join( '|' )
 				},
 				function( data ) {
 					var fail = false;
-					
+
 					if ( data.error ) {
 						handleError( listItem, pageName, { info: mediaWiki.msg( 'push-tab-err-filepush', data.error.info ) } );
-						fail = true;						
+						fail = true;
 					}
 					else {
 						for ( i in data ) {
@@ -191,42 +191,42 @@
 							else if ( !data[i].upload ) {
 								handleError( listItem, pageName, { info: mediaWiki.msg( 'push-tab-err-filepush-unknown' ) } );
 								fail = true;
-								break;						
+								break;
 							}
-						}						
+						}
 					}
-					
+
 					if ( !fail ) {
 						startFilePush( pageName, images, targetOffset, listItem, fileName );
 					}
 				}
-			);			
+			);
 		}
 		else {
 			if ( images.length > 0 ) {
 				var currentFile = images.pop();
-				startFilePush( pageName, images, 0, listItem, currentFile );				
+				startFilePush( pageName, images, 0, listItem, currentFile );
 			}
 			else {
 				completeItem( pageName, listItem );
 			}
 		}
-	}	
-	
+	}
+
 	function completeItem( pageName, listItem ) {
 		listItem.text( mediaWiki.msg( 'push-special-item-completed', pageName ) );
 		listItem.css( 'color', 'darkgray' );
-		initiateNextPush();			
+		initiateNextPush();
 	}
-	
+
 	function handleError( listItem, pageName, error ) {
 		listItem.text( mediaWiki.msg( 'push-special-item-failed', pageName, error.info ) );
 		listItem.css( 'color', 'darkred' );
-		initiateNextPush();	
-	}	
-	
+		initiateNextPush();
+	}
+
 	function showCompletion() {
 		appendAndScroll( $( '<li />' ).append( $( '<b />' ).text( mediaWiki.msg( 'push-special-push-done' ) ) ) );
 	}
-	
+
 } ); })(jQuery);
--- modules/ext.push.tab.js
+++ modules/ext.push.tab.js
@@ -1,43 +1,43 @@
 /**
  * JavaScript for the Push tab in the Push extension.
  * @see http://www.mediawiki.org/wiki/Extension:Push
- * 
+ *
  * @author Jeroen De Dauw <jeroendedauw at gmail dot com>
  */
 
 (function( $ ) { $( document ).ready( function() {
-	
+
 	// Compatibility with pre-RL code.
 	// Messages will have been loaded into wgPushMessages.
 	if ( typeof mediaWiki === 'undefined' ) {
 		mediaWiki = new Object();
-		
+
 		mediaWiki.msg = function() {
 			message = window.wgPushMessages[arguments[0]];
-			
+
 			for ( var i = arguments.length - 1; i > 0; i-- ) {
 				message = message.replace( '$' + i, arguments[i] );
 			}
-			
+
 			return message;
 		}
 	}
-	
+
 	mw = mediaWiki;
-	
+
 	var pages;
 	var targetData = [];
-	
+
 	$.each($(".push-button"), function(i,v) {
 		getRemoteArticleInfo( $(v).attr( 'targetid' ), $(v).attr( 'pushtarget' ) );
-	});	
-	
+	});
+
 	$('.push-button').click(function() {
 		this.disabled = true;
-		
+
 		var errorDiv = $( '#targeterrors' + $(this).attr( 'targetid' ) );
-		errorDiv.fadeOut( 'fast' );			
-		
+		errorDiv.fadeOut( 'fast' );
+
 		if ( $('#checkIncTemplates').attr('checked') ) {
 			pages = window.wgPushTemplates;
 			pages.unshift( $('#pageName').attr('value') );
@@ -45,7 +45,7 @@
 		else {
 			pages = [$('#pageName').attr('value')];
 		}
-		
+
 		setButtonToImgPush(
 			this,
 			pages,
@@ -53,24 +53,24 @@
 			$(this).attr( 'targetname' )
 		);
 	});
-	
+
 	$('#push-all-button').click(function() {
 		this.disabled = true;
 		this.innerHTML = mw.msg( 'push-button-pushing' );
 		$.each($(".push-button"), function(i,v) {
 			$(v).click();
 		});
-	});	
-	
+	});
+
 	$('#divIncTemplates').hover(
 		function() {
 			var isHidden = $('#txtTemplateList').css( 'opacity' ) == 0;
-			
+
 			if ( isHidden ) {
 				$('#txtTemplateList').css( 'display', 'inline' );
 			}
-			
-			$('#txtTemplateList').fadeTo( 
+
+			$('#txtTemplateList').fadeTo(
 				isHidden? 'slow' : 'fast',
 				1
 			);
@@ -79,26 +79,26 @@
 			$('#txtTemplateList').fadeTo( 'fast', 0.5 )
 		}
 	);
-	
+
 	$('#divIncTemplates').click(function() {
 		setIncludeFilesText();
 		displayTargetsConflictStatus();
 	});
-	
+
 	$('#divIncFiles').click(function() {
 		displayTargetsConflictStatus();
-	});	
-	
+	});
+
 	$('#divIncFiles').hover(
 		function() {
 			var isHidden = $('#txtFileList').css( 'opacity' ) == 0;
-			
+
 			if ( isHidden ) {
 				$('#txtFileList').css( 'display', 'inline' );
 				setIncludeFilesText();
 			}
-			
-			$('#txtFileList').fadeTo( 
+
+			$('#txtFileList').fadeTo(
 					isHidden ? 'slow' : 'fast',
 				1
 			);
@@ -106,8 +106,8 @@
 		function() {
 			$('#txtFileList').fadeTo( 'fast', 0.5 )
 		}
-	);	
-	
+	);
+
 	function setIncludeFilesText() {
 		if ( $('#checkIncFiles').length != 0 ) {
 			var files = window.wgPushPageFiles;
@@ -118,12 +118,12 @@
 
 			if ( files.length > 0 ) {
 				$('#txtFileList').text( '(' + mw.msg( 'push-tab-embedded-files' ) + ' ' );
-				
+
 				for ( i in files ) {
 					if ( i > 0 ) $('#txtFileList').append( ', ' );
 					$('#txtFileList').append( $( '<a>' ).attr( 'href', window.wgPushIndexPath + '?title=' + files[i] ).text( files[i] ) );
 				}
-				
+
 				$('#txtFileList').append( ')' );
 			}
 			else {
@@ -131,10 +131,10 @@
 			}
 		}
 	}
-	
+
 	function getRemoteArticleInfo( targetId, targetUrl ) {
 		var pageName = $('#pageName').attr('value');
-		
+
 		$.getJSON(
 			targetUrl + '/api.php?callback=?',
 			{
@@ -154,23 +154,23 @@
 
 					var existingPages = [];
 					var remotePage = false;
-					
+
 					for ( remotePageId in data.query.pages ) {
 						if ( remotePageId > 0 ) {
 							if ( data.query.pages[remotePageId].title == pageName ) {
-								remotePage = data.query.pages[remotePageId]; 
+								remotePage = data.query.pages[remotePageId];
 							}
 							else {
 								existingPages.push( data.query.pages[remotePageId] );
 							}
 						}
 					}
-					
+
 					targetData[targetId] = { 'existingPages': existingPages };
-					
+
 					if ( remotePage ) {
 						$( '#targetlink' + targetId ).attr( {'class': ''} );
-						
+
 						var revision = remotePage.revisions[0];
 						var dateTime = revision.timestamp.split( 'T' );
 
@@ -179,44 +179,44 @@
 							revision.user,
 							dateTime[0],
 							dateTime[1].replace( 'Z', '' )
-						);						
+						);
 					}
 					else {
 						$( '#targetlink' + targetId ).attr( {'class': 'new'} );
 						var message = mw.msg( 'push-tab-not-created' );
 					}
-					
+
 					infoDiv.text( message );
 					infoDiv.fadeIn( 'slow' );
-					
+
 					displayTargetConflictStatus( targetId );
 				}
 			}
 		);
 	}
-	
+
 	function displayTargetsConflictStatus() {
 		$.each($(".push-button"), function(i,v) {
 			displayTargetConflictStatus( $(v).attr( 'targetid' ) );
-		});		
+		});
 	}
-	
+
 	function displayTargetConflictStatus( targetId ) {
 		if ( !targetData[targetId] ) {
 			// It's possible the request to retrieve this data failed, so don't do anything when this is the case.
 			return;
 		}
-		
+
 		if ( $('#checkIncTemplates').attr('checked') ) {
 			var overideTemplates = [];
-			
+
 			for ( remotePageId in targetData[targetId].existingPages ) {
 				if ( targetData[targetId].existingPages[remotePageId].ns == 10 ) {
 					// Add the template, but get rid of the namespace prefix first.
 					overideTemplates.push( targetData[targetId].existingPages[remotePageId].title.split( ':', 2 )[1] );
 				}
 			}
-			
+
 			if ( overideTemplates.length > 0 ) {
 				$( '#targettemplateconflicts' + targetId )
 					.text( mw.msg( 'push-tab-template-override',
@@ -231,18 +231,18 @@
 		}
 		else {
 			$( '#targettemplateconflicts' + targetId ).fadeOut( 'fast' );
-		}		
-		
+		}
+
 		if ( $('#checkIncFiles').length != 0 && $('#checkIncFiles').attr('checked') ) {
 			var overideFiles = [];
-			
+
 			for ( remotePageId in targetData[targetId].existingPages ) {
 				if ( targetData[targetId].existingPages[remotePageId].ns == 6 ) {
 					// Add the file, but get rid of the namespace prefix first.
 					overideFiles.push( targetData[targetId].existingPages[remotePageId].title.split( ':', 2 )[1] );
 				}
 			}
-			
+
 			if ( overideFiles.length > 0 ) {
 				$( '#targetfileconflicts' + targetId )
 					.text( mw.msg( 'push-tab-files-override',
@@ -253,16 +253,16 @@
 			}
 			else {
 				$( '#targetfileconflicts' + targetId ).fadeOut( 'slow' );
-			}			
+			}
 		}
 		else {
 			$( '#targetfileconflicts' + targetId ).fadeOut( 'fast' );
-		}		
+		}
 	}
-	
+
 	function initiatePush( sender, pages, targetUrl, targetName ) {
 		sender.innerHTML = mw.msg( 'push-button-pushing' );
-		
+
 		$.getJSON(
 			wgScriptPath + '/api.php',
 			{
@@ -282,20 +282,20 @@
 					handlePushingCompletion( sender, targetUrl, targetName );
 				}
 			}
-		); 	
+		);
 	}
-	
+
 	function handlePushingCompletion( sender, targetUrl, targetName ) {
 		sender.innerHTML = mw.msg( 'push-button-completed' );
-		
+
 		setTimeout( function() {
 			reEnableButton( sender, targetUrl, targetName );
-		}, 1000 );	
+		}, 1000 );
 	}
-	
+
 	function setButtonToImgPush( button, pages, targetUrl, targetName ) {
 		var images = window.wgPushPageFiles.concat( window.wgPushTemplateFiles );
-		
+
 		if ( images.length > 0 && $('#checkIncFiles').length != 0 && $('#checkIncFiles').attr('checked') ) {
 			var currentFile = images.pop();
 			button.innerHTML = mw.msg( 'push-button-pushing-files' );
@@ -305,23 +305,23 @@
 			initiatePush( button, pages, targetUrl, targetName );
 		}
 	}
-	
+
 	function initiateImagePush( sender, pages, targetUrl, targetName, images, fileName ) {
 		$.getJSON(
 			wgScriptPath + '/api.php',
 			{
 				'action': 'pushimages',
 				'format': 'json',
-				'images': fileName, 
+				'images': fileName,
 				'targets': targetUrl
 			},
 			function( data ) {
 				var fail = false;
-				
+
 				if ( data.error ) {
 					data.error.info = mw.msg( 'push-tab-err-filepush', data.error.info );
 					handleError( sender, targetUrl, data.error );
-					fail = true;					
+					fail = true;
 				}
 				else {
 					for ( i in data ) {
@@ -335,9 +335,9 @@
 							data[i].error.info = mw.msg( 'push-tab-err-filepush-unknown' );
 							handleError( sender, targetUrl, data[i].error );
 							fail = true;
-							break;						
+							break;
 						}
-					}					
+					}
 				}
 
 				if ( !fail ) {
@@ -350,47 +350,47 @@
 					}
 				}
 			}
-		);			
+		);
 	}
-	
+
 	function reEnableButton( button, targetUrl, targetName ) {
 		button.innerHTML = mw.msg( 'push-button-text' );
 		button.disabled = false;
-		
+
 		getRemoteArticleInfo( $(button).attr( 'targetid' ), $(button).attr( 'pushtarget' ) );
-		
+
 		var pushAllButton = $('#push-all-button');
-		
+
 		// If there is a "push all" button, make sure to reset it
 		// when all other buttons have been reset.
 		if ( typeof pushAllButton !== "undefined" ) {
 			var hasDisabled = false;
-			
+
 			$.each($(".push-button"), function(i,v) {
 				if ( v.disabled ) {
 					hasDisabled = true;
 				}
 			});
-			
+
 			if ( !hasDisabled ) {
 				pushAllButton.attr( "disabled", false );
 				pushAllButton.text( mw.msg( 'push-button-all' ) );
-			}			
+			}
 		}
-	}	
-	
+	}
+
 	function handleError( sender, targetUrl, error ) {
 		var errorDiv = $( '#targeterrors' + $(sender).attr( 'targetid' ) );
 
 		if ( error.code && error.code == 'uploaddisabled' ) {
 			error.info = mw.msg( 'push-tab-err-uploaddisabled' );
 		}
-		
+
 		errorDiv.text( error.info );
-		errorDiv.fadeIn( 'slow' );			
-		
-		sender.innerHTML = mw.msg( 'push-button-failed' );	
+		errorDiv.fadeIn( 'slow' );
+
+		sender.innerHTML = mw.msg( 'push-button-failed' );
 		setTimeout( function() {reEnableButton( sender );}, 2500 );
 	}
-	
+
 } ); })( jQuery );
