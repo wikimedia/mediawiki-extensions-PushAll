5e9334c0b409ba77e7015267f0f88a0ccfc6551a
Reedy <reedy@wikimedia.org>
2018-06-21 21:58:15 +0100

    Extension registration!
    Change-Id: I7e787e33924df1ba01040f43dca726ca29a8c6ba

--- Push.php
+++ Push.php
@@ -1,7 +1,5 @@
 <?php
 /**
- * Initialization file for the Push extension.
- *
  * Documentation:	 		http://www.mediawiki.org/wiki/Extension:Push
  * Support					http://www.mediawiki.org/wiki/Extension_talk:Push
  * Source code:             http://svn.wikimedia.org/viewvc/mediawiki/trunk/extensions/Push
@@ -13,108 +11,16 @@
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 
-/**
- * This documentation group collects source code files belonging to Push.
- *
- * @defgroup Push Push
- */
-
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( 'Not an entry point.' );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'Push' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['Push'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['PushAliases'] = __DIR__ . '/Push.alias.php';
+	wfWarn(
+		'Deprecated PHP entry point used for Push extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
 }
 
-define( 'Push_VERSION', '1.3.0 alpha' );
-
-$wgExtensionCredits['other'][] = [
-	'path' => __FILE__,
-	'name' => 'Push',
-	'version' => Push_VERSION,
-	'author' => [
-		'[http://www.mediawiki.org/wiki/User:Jeroen_De_Dauw Jeroen De Dauw] for [http://www.wikiworks.com WikiWorks]',
-	],
-	'url' => 'https://www.mediawiki.org/wiki/Extension:Push',
-	'descriptionmsg' => 'push-desc'
-];
-
-$egPushScriptPath = (
-	isset( $wgExtensionAssetsPath ) && $wgExtensionAssetsPath
-		? $wgExtensionAssetsPath
-		: $wgScriptPath . '/extensions'
-	) . '/Push';
-$egPushIP = __DIR__;
-
-$wgMessagesDirs['Push'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['PushAlias'] 			= $egPushIP . '/Push.alias.php';
-
-$wgAutoloadClasses['PushHooks'] 			= $egPushIP . '/Push.hooks.php';
-$wgAutoloadClasses['ApiPushBase'] 			= $egPushIP . '/api/ApiPushBase.php';
-$wgAutoloadClasses['ApiPush'] 				= $egPushIP . '/api/ApiPush.php';
-$wgAutoloadClasses['ApiPushImages'] 		= $egPushIP . '/api/ApiPushImages.php';
-$wgAutoloadClasses['PushTab'] 				= $egPushIP . '/includes/Push_Tab.php';
-$wgAutoloadClasses['PushFunctions'] 		= $egPushIP . '/includes/Push_Functions.php';
-$wgAutoloadClasses['SpecialPush'] 			= $egPushIP . '/specials/Push_Body.php';
-
-$wgSpecialPages['Push'] = 'SpecialPush';
-
-$wgAPIModules['push'] = 'ApiPush';
-$wgAPIModules['pushimages'] = 'ApiPushImages';
-
-$wgHooks['UnknownAction'][] = 'PushTab::onUnknownAction';
-$wgHooks['SkinTemplateTabs'][] = 'PushTab::displayTab';
-$wgHooks['SkinTemplateNavigation'][] = 'PushTab::displayTab2';
-
-$wgHooks['AdminLinks'][] = 'PushHooks::addToAdminLinks';
-
-$wgAvailableRights[] = 'push';
-$wgAvailableRights[] = 'pushadmin';
-$wgAvailableRights[] = 'filepush';
-$wgAvailableRights[] = 'bulkpush';
-
-$egPushJSMessages = [
-	'push-button-pushing',
-	'push-button-completed',
-	'push-button-failed',
-	'push-import-revision-message',
-	'push-button-text',
-	'push-button-all',
-	'push-special-item-pushing',
-	'push-special-item-completed',
-	'push-special-item-failed',
-	'push-special-push-done',
-	'push-err-captacha',
-	'push-tab-last-edit',
-	'push-tab-not-created',
-	'push-err-captcha-page',
-	'push-button-pushing-files',
-	'push-special-err-imginfo-failed',
-	'push-special-obtaining-fileinfo',
-	'push-special-pushing-file',
-	'push-tab-err-fileinfo',
-	'push-tab-err-filepush',
-	'push-tab-err-filepush-unknown',
-	'push-tab-embedded-files',
-	'push-tab-no-embedded-files',
-	'push-tab-files-override',
-	'push-tab-template-override',
-	'push-tab-err-uploaddisabled'
-];
-
-$moduleTemplate = [
-	'localBasePath' => __DIR__,
-	'remoteBasePath' => $egPushScriptPath,
-	'group' => 'ext.push'
-];
-
-$wgResourceModules['ext.push.tab'] = $moduleTemplate + [
-	'scripts' => 'includes/ext.push.tab.js',
-	'dependencies' => [ 'mediawiki.jqueryMsg' ],
-	'messages' => $egPushJSMessages
-];
-
-$wgResourceModules['ext.push.special'] = $moduleTemplate + [
-	'scripts' => 'specials/ext.push.special.js',
-	'dependencies' => [],
-	'messages' => $egPushJSMessages
-];
-
-require_once 'Push_Settings.php';
+die( 'This version of the Push extension requires MediaWiki 1.29+' );
